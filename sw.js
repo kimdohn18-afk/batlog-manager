3722be42657a94174b64d3f912c260883d579953
kimdohn18-afk <kimdohn18@gmail.com>
2026-03-29 09:36:35 +0900

    sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,25 @@
+var CACHE_VERSION = 'v2';
+
 self.addEventListener('install', function(e){
   self.skipWaiting();
 });
+
+self.addEventListener('activate', function(e){
+  e.waitUntil(
+    caches.keys().then(function(names){
+      return Promise.all(
+        names.map(function(name){
+          return caches.delete(name);
+        })
+      );
+    })
+  );
+});
+
 self.addEventListener('fetch', function(e){
-  e.respondWith(fetch(e.request));
+  e.respondWith(
+    fetch(e.request).catch(function(){
+      return caches.match(e.request);
+    })
+  );
 });
